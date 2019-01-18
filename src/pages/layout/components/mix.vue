53099dfed9ad7b962d8f9b2a6bac3f48f3ecc0ca
shenzq <shenzq@citycloud.com.cn>
2019-01-18 17:39:51 +0800

    feat(mix.vue): test

--- src/pages/layout/components/mix.vue
+++ src/pages/layout/components/mix.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-row :gutter="20">
-        <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+        <el-col :span="16"><div class="grid-content bg-purple">222</div></el-col>
         <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
     </el-row>
     <el-row :gutter="40">
